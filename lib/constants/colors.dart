750b0a8bc1bc18641e1e04566fbde9550c4d0be0
Qharny <cannycc1020@gmail.com>
2023-10-23 15:16:18 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   asset/logo.jpg
    	new file:   lib/constants/colors.dart
    	modified:   pubspec.yaml

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+// my various colors to be used in my code
+
+import 'package:flutter/material.dart';
+
+// color red
+const Color tdRed = Color(0xFFDA4040);
+// color blue
+const Color tdBlue = Color(0xFF2196F3);
+// black
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
+const Color tdBGColor = Color(0xFFEEEFF5);
