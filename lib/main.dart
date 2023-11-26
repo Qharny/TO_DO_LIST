381a592fe2105e448aa3e9e102617d11b6703d03
Qharny <cannycc1020@gmail.com>
2023-11-26 23:30:34 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch and 'origin/main' have diverged,
    and have 2 and 1 different commits each, respectively.
    
    Changes to be committed:
    	new file:   FirstTry.txt
    	new file:   assets/icon/avatar.jpg
    	modified:   assets/icon/logo.jpg
    	modified:   lib/main.dart
    	modified:   lib/screens/home.dart
    	modified:   lib/widgets/items.dart
    	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:to_do_list/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,106 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false, // removes the debug banner
       title: 'TO DO LIST',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
-            .copyWith(secondary: Colors.deepOrange),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'TO DO APPLICATION'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> items = ['First Task', 'Second Task'];
-  TextEditingController textEditingController = TextEditingController();
-
-  void addItem() {
-    setState(() {
-      items.add(textEditingController.text);
-      textEditingController.clear();
-    });
-  }
-
-  void removeItem(int index) {
-    setState(() {
-      items.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[700],
-        centerTitle: true,
-        title: Text(
-          widget.title,
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) => Card(
-          child: ListTile(
-            leading: IconButton(
-              icon: Icon(Icons.check_box_outline_blank),
-              onPressed: () {},
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                removeItem(index);
-              },
-            ),
-            title: Text(items[index]),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add task',
-        child: Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (_) {
-              return AlertDialog(
-                content: TextField(
-                  controller: textEditingController,
-                  decoration: InputDecoration(labelText: 'Enter your task'),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text('CANCEL'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  TextButton(
-                    child: Text('ADD'),
-                    onPressed: () {
-                      addItem();
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              );
-            },
-          );
-        },
-      ),
+      home: Home(),
     );
   }
 }
